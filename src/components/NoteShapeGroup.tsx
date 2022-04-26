969d8b9841b1dee008dc1415e208a01d03b9c580
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2022-04-26 23:19:45 +1000

    feat: add munted tie

--- src/components/NoteShapeGroup.tsx
+++ src/components/NoteShapeGroup.tsx
@@ -2,8 +2,19 @@ import { maxNoteXLength } from "src/constants/svg";
 import { BaseDuration, Duration } from "src/types";
 import { NoteShapePath } from "./NoteShapePath";
 
-const TiePath = () => {
-  return <div>(</div>;
+interface TiePathProps {
+  opacity?: number;
+
+  handleClick?: () => void;
+  color: string;
+  xStart: number;
+  yStart: number;
+  xEnd: number;
+  yEnd: number;
+}
+
+const TiePath = ({ xStart, yStart, xEnd, yEnd, color, opacity, handleClick }: TiePathProps) => {
+  return <path fill={color} opacity={opacity} onClick={handleClick} d={`M ${xStart} ${yStart} A 50 10 0 0 1 ${xEnd} ${yEnd}`} />;
 };
 
 interface NoteShapeGroupProps {
@@ -28,11 +39,15 @@ export const NoteShapeGroup = ({
   let index = 0;
   for (const [baseDuration, multiplier] of Object.entries(durationObject)) {
     if (index > 0) {
-      buffer.push(<TiePath />);
+      buffer.push(<TiePath handleClick={handleClick}
+        color={color}
+        opacity={opacity} xStart={baseXPosition + (groupCounter - 1) * maxNoteXLength} yStart={baseYPosition} xEnd={baseXPosition + (groupCounter) * maxNoteXLength} yEnd={baseYPosition} />);
     }
     for (let n = 0; n < multiplier; n++) {
       if (n > 0) {
-        buffer.push(<TiePath />);
+        buffer.push(<TiePath handleClick={handleClick}
+          color={color}
+          opacity={opacity} xStart={baseXPosition + (groupCounter - 1) * maxNoteXLength} yStart={baseYPosition} xEnd={baseXPosition + (groupCounter) * maxNoteXLength} yEnd={baseYPosition} />);
       }
       buffer.push(
         <NoteShapePath
