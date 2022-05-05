2fc69c39bd30c2948cbef30de72ac5b088ded2d4
mitchellroberts11 <mitchell.roberts@iress.com>
2022-05-05 13:26:16 +1000

    ui: add margins to modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import { useStore } from 'src/gameStore';
+import React from "react";
+import { useStore } from "src/gameStore";
 import "./Modal.css";
 
 interface ModalProps {
-    title: string;
-    children?: React.ReactNode;
+  title: string;
+  children?: React.ReactNode;
 }
 
 export const Modal: React.FC<ModalProps> = ({ title, children }) => {
-    const { showInstructions, toggleInstructions } = useStore();
-    console.log(showInstructions)
-    return (
-        <div className={showInstructions ? "visible-modal" : "invisible-modal"} >
-            <div className="modal-content" >
-                <div className="modal-header" >
-                    <h3 className="modal-title">{title}</h3>
-                </div>
-                <div className="modal-body">
-                    {children}
-                </div>
-                <div className="modal-footer">
-                    <button className="modal-button" onClick={() => toggleInstructions()}>Close</button>
-                </div>
-            </div>
+  const { showInstructions, toggleInstructions } = useStore();
+  console.log(showInstructions);
+  return (
+    <>
+      {showInstructions && <div className="grey-background" />}
+      <div className={showInstructions ? "visible-modal" : "invisible-modal"}>
+        <div className="modal-content">
+          <div className="modal-header">
+            <h3 className="modal-title">{title}</h3>
+          </div>
+          <div className="modal-body">{children}</div>
+          <div className="modal-footer">
+            <button
+              className="modal-button"
+              onClick={() => toggleInstructions()}
+            >
+              Close
+            </button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
