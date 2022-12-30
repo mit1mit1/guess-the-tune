939bfb07d03f656cac5e996aab5f02621a318f8c
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2022-12-31 01:17:18 +1100

    feat: export a link with hashed song json

--- src/components/OutputModal.tsx
+++ src/components/OutputModal.tsx
@@ -1,18 +1,27 @@
 import { chosenSong } from "src/constants/game";
 import { useStore } from "src/gameStore";
 import { Modal } from "./Modal";
+import CryptoJS from "crypto-js";
 
 export const OutputModal = () => {
   const { showOutput, toggleOutputModal, guesses } = useStore();
   const song = chosenSong;
   song.notes = guesses;
+  const encryptedString = CryptoJS.AES.encrypt(JSON.stringify(song), "Secret Passphrase").toString();
+  let path = window.location.href.split('?')[0]
+  let queryString = new URLSearchParams({ songHash: encryptedString }).toString()
   return (
     <Modal
       title="Song JSON"
       visible={showOutput}
       toggleVisible={toggleOutputModal}
     >
-      {JSON.stringify(song)}
-    </Modal>
+      <div>
+        JSON: {JSON.stringify(song)}
+      </div>
+      <a href={`${path}?${queryString}`}>
+        {path} ? {queryString}
+      </a>
+    </Modal >
   );
 };
--- src/constants/game.ts
+++ src/constants/game.ts
@@ -2,6 +2,8 @@ import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone"; // dependent on utc plugin
 import { gameSongs } from "src/gameSongs";
+import CryptoJS from "crypto-js";
+
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -29,10 +31,25 @@ export const availableSongs = useUnreadySongs
   ? gameSongs
   : gameSongs.filter((gameSong) => !!gameSong.readyForProduction);
 
-export const availableIndices = availableSongs.map(availableSong => gameSongs.indexOf(availableSong));
+export const availableIndices = availableSongs.map((availableSong) =>
+  gameSongs.indexOf(availableSong)
+);
 
-export const chosenSong =
+export const queryParamSongHash = new URLSearchParams(
+  window.location.search
+).get("songHash");
+
+export let chosenSong =
   availableSongs[Math.abs(songIndex % availableSongs.length)];
+
+if (queryParamSongHash) {
+  chosenSong = JSON.parse(
+    CryptoJS.AES.decrypt(
+      queryParamSongHash.toString(),
+      "Secret Passphrase"
+    ).toString(CryptoJS.enc.Utf8)
+  );
+}
 
 export const chosenSongIndex = gameSongs.indexOf(chosenSong);
 
