776f4eedf7bd1a50db6e2067a351f6d1141f5852
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2023-01-01 19:43:52 +1100

    tweak: link copying

--- src/components/OutputModal.tsx
+++ src/components/OutputModal.tsx
@@ -3,20 +3,24 @@ import { useStore } from "src/gameStore";
 import { Modal } from "./Modal";
 import { useState } from "react";
 import { encodeSong } from "src/utils/encoder";
+import styles from "./App.module.scss";
 
 export const OutputModal = () => {
   const { showOutput, toggleOutputModal, guesses, bpm, overrideBPM } = useStore();
   const song = chosenSong;
   const [songName, setSongName] = useState(chosenSong.name);
+  const [copied, setCopied] = useState(false);
   song.notes = guesses;
   let encryptedString = encodeSong({ ...song, name: songName, bpm });
   let path = window.location.href.split('?')[0]
   let queryString = new URLSearchParams({ songHash: encryptedString }).toString()
-  if (showOutput) {
-    console.log(JSON.stringify(song))
-  }
   const copyLinkToClipboard = () => {
-    navigator.clipboard.writeText(`Try and guess the tune I wrote with Musicle: ${path}?${queryString}`);
+    if (showOutput) {
+      console.log(JSON.stringify(song))
+    }
+    navigator.clipboard.writeText(`${path}?${queryString}`);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1500)
   }
   return (
     <Modal
@@ -24,17 +28,31 @@ export const OutputModal = () => {
       visible={showOutput}
       toggleVisible={toggleOutputModal}
     >
-      <div>
-        <label htmlFor="customSongName">Name: </label>
-        <input id="customSongName" type="text" name="customSongName" onChange={(e) => setSongName(e.target.value)} value={songName} />
+      <div className={styles.formRow}>
+        <div>
+          <label htmlFor="customSongName">Name: </label>
+        </div>
+        <div>
+          <input id="customSongName" type="text" name="customSongName" onChange={(e) => {
+            setSongName(e.target.value);
+            setCopied(false)
+          }} value={songName} />
+        </div>
       </div>
-      <div>
-        <label htmlFor="customSongBPM">BPM: </label>
-        <input id="customSongBPM" type="number" name="customSongBPM" onChange={(e) => overrideBPM(parseInt(e.target.value))} value={bpm} />
+      <div className={styles.formRow}>
+        <div>
+          <label htmlFor="customSongBPM">BPM: </label>
+        </div>
+        <div>
+          <input id="customSongBPM" type="number" name="customSongBPM" onChange={(e) => {
+            overrideBPM(parseInt(e.target.value));
+            setCopied(false)
+          }} value={bpm} />
+        </div>
       </div>
-      <button onClick={copyLinkToClipboard}>
+      <button className={styles.button} onClick={copyLinkToClipboard}>
         Copy link
-      </button>
+      </button>{copied && "Copied!"}
     </Modal >
   );
 };
